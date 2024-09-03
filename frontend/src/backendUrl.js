7cdde385000de3850f82605c00924b1a23a17960
Arijit655 <arijitbarik655@gmail.com>
2024-09-03 19:48:12 +0530

    frontend-updated-arijit

--- frontend/src/backendUrl.js
+++ frontend/src/backendUrl.js
@@ -1,4 +1,4 @@
-// const url = "https://finance-app-backend-1i1k.onrender.com"
-const url = "http://localhost:5000"
+const url = "https://dashboard.render.com/web/srv-crbh78ij1k6c738bngkg"
+// const url = "http://localhost:5000"
 
 export default url
