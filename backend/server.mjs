7cefd2f66c24d41625b2e9deffc657423cf6d2f2
Arijit655 <arijitbarik655@gmail.com>
2024-09-03 21:31:56 +0530

    cors update

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,7 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser';
-import connectDB from './config/db.mjs';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.mjs";
 import cors from "cors";
 const app = express();
 const PORT = 5000;
@@ -11,25 +11,28 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(cors({
-//     // origin: ['https://finance-tracker-frontend-diit.onrender.com', 'http://localhost:3000'],
-//     origin: '*',
-//     methods: ['POST', 'GET', 'PUT', 'DELETE', 'PATCH'],
-//     credentials: true
-// }));
-app.use(cors());
-app.get("/",(req,res)=>{
-  res.setHeader("Access-Control-Allow-Credential","true");
-  res.send("Api is running well baby-arijit");
-})
+app.use(
+    cors({
+        origin: [
+            "https://finance-edited-frontend.onrender.com"
+        ],
+        // origin: '*',
+        methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
+        credentials: true,
+    })
+);
+// app.use(cors());
+app.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Credential", "true");
+    res.send("Api is running well baby-arijit");
+});
 
-
-import userRoutes from './routes/userRoutes.mjs'
-import txnRoutes from './routes/txnRoutes.mjs'
-import checkRoutes from './routes/checkRoutes.mjs'
+import userRoutes from "./routes/userRoutes.mjs";
+import txnRoutes from "./routes/txnRoutes.mjs";
+import checkRoutes from "./routes/checkRoutes.mjs";
 app.use("/user", userRoutes);
 app.use("/txns", txnRoutes);
-app.use("/check",checkRoutes);
+app.use("/check", checkRoutes);
 
 // app.get('/logout', (req, res) => {
 //   res.redirect('/auth');
@@ -37,5 +40,5 @@ app.use("/check",checkRoutes);
 // });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`)
+    console.log(`Server listening on PORT ${PORT}`);
 });
