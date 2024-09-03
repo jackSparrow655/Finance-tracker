cae0b9edfb227367f718149c0fff3399a445c390
ARIJIT BARIK <121745672+jackSparrow655@users.noreply.github.com>
2024-09-03 19:22:43 +0530

    Update backendUrl.js
    update the backend url with deployed url

--- frontend/src/backendUrl.js
+++ frontend/src/backendUrl.js
@@ -1,4 +1,5 @@
 // const url = "https://finance-app-backend-1i1k.onrender.com"
-const url = "http://localhost:5000"
+//const url = "http://localhost:5000"
+const url = "https://finance-tracker-backend-8az1.onrender.com"
 
 export default url
